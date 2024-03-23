e44cba67bc7a7653e5e48b85104fefdcd5134eca
Brayan Tamiche <brayan.tamiche.g.dbb@gmail.com>
2024-03-23 11:50:01 -0400

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+import Card from "../components/Card.vue"
+
+let character = ref([])
+
+async function loadCharacters(){
+  const response = await fetch(`https://rickandmortyapi.com/api/character?page=1`)
+  const data = await response.json()
+  character.value = data.results
+  console.log(data)
+}
+loadCharacters()
+
+</script>
+
+<template>
+  <div class="flex justify-center mt-10">
+    <h1 class="text-5xl ">Personajes de Rick y Morty</h1>
+  </div>
+    <div v-for="personajes in character" :key="character.id">
+   {{  personajes.name }} 
+    </div>
+
+  <!--<Card :character="character.value"/> -->
+
+</template>
